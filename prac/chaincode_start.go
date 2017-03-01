d9d84925aad6ebe73b6f16070909a3a737d94b47
harisahmed321 <harisahmed321@gmail.com>
2017-03-01 18:24:08 +0500

    - update

--- prac/chaincode_start.go
+++ prac/chaincode_start.go
@@ -23,12 +23,12 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
-	A := args[0]
-	val := args[1]
-	err := stub.PutState(A, []byte(val))
+
+	err := stub.PutState(args[0], []byte(args[1]))
 	if err != nil {
 		return nil, err
 	}
+
 	return nil, nil
 }
 
@@ -39,6 +39,8 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" { //initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	} else if function == "write" {
+		return t.write(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function) //error
 
@@ -47,55 +49,49 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 
 // Query is our entry point for queries
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("query is running " + function)
 
-	var key string
+	// Handle different functions
+	if function == "read" {
+		return t.read(stub, args)
+	}
+	fmt.Println("query did not find func: " + function) //error
+
+	return nil, errors.New("Received unknown function query: " + function)
+}
+
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, value string
 	var err error
-	key = args[0]
-	Avalbytes, err := stub.GetState(key)
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+	}
+
+	key = args[0] //rename for fun
+	value = args[1]
+	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
 	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, err
+	}
+	return nil, nil
+}
+
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
 
-	return Avalbytes, nil
-
+	return valAsbytes, nil
 }
-
-// func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-// 	var key, jsonResp string
-
-// 	key = args[0]
-// 	err := stub.GetState(key)
-// }
-
-// // Check user
-// func (t *SimpleChaincode) AddUser(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-
-// 	var A string // Entities
-// 	var err error
-
-// 	if len(args) != 1 {
-// 		return nil, errors.New("Incorrect number of arguments. Expecting name of the person to query")
-// 	}
-
-// 	user = args[0]
-
-// 	// Get the state from the ledger
-// 	checkExistingUser, err := stub.GetState(user)
-// 	if err != nil {
-// 		jsonResp := "{\"Error\":\"Failed to get state for user " + user + "\"}"
-// 		return nil, errors.New(jsonResp)
-// 	}
-
-// 	if checkExistingUser == nil {
-// 		createUser, err := stub.PutState(user)
-// 		if err != null {
-// 			jsonResp := "{\"Error\":\"Failed to create user \"}"
-// 			return nil, errors.New(jsonResp)
-// 		}
-// 		jsonResp := "{\"Name\":\"" + user + "\"}"
-// 		fmt.Printf("Query Response:%s\n", jsonResp)
-// 		return user, nil
-// 	}
-
-// }
