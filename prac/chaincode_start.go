ec61155e23fbd4c4f3a87d1638c75d8e1ba1c05a
harisahmed321 <harisahmed321@gmail.com>
2017-03-02 21:40:31 +0500

    -

--- prac/chaincode_start.go
+++ prac/chaincode_start.go
@@ -47,10 +47,10 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	{
 		return t.addUser(stub, args)
 	} 
-	if function == "write" 
-	{
-		return t.write(stub, args)
-	}
+	// if function == "write" 
+	// {
+	// 	return t.write(stub, args)
+	// }
 		fmt.Println("invoke did not find func: " + function) //error
 
 	return nil, errors.New("Received unknown function invocation: " + function)
